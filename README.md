e9e48ba4e3e74ed43b02638511df6208b87b03c8
sturat <wqiosa@163.com>
2018-12-12 14:30:53 +0800

    update object use, now it can print object content.

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 ## In a simple way, let your node command line print out a colored log.
 
-错误 message...
-成功 message...
-警告 message...
-信息 message...
+
+## Install
+
+```console
+$ npm install simple-color-console
+```
+- 错误 message...
+- 成功 message...
+- 警告 message...
+- 信息 message...
+
+## Usage
+
+```js
+const log = require('simple-color-console');
+
+log.error('message...');
+log.success('message...');
+log.warn('message...');
+log.info('message...');
+log.info(object);
+```
--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const chalk = require('chalk');
 const print = console.log;
 const log = {
     info: (msg) => {
-        print(chalk.bgBlue.black('信息'), chalk.blue(msg));
+        print(chalk.bgBlue.black('信息'), printLog(msg,'blue'));
     },
     warn: (msg) => {
-        print(chalk.bgYellow.black('警告'), chalk.yellow(msg));
+        print(chalk.bgYellow.black('警告'), printLog(msg,'yellow'));
     },
     error: (msg) => {
-        print(chalk.bgRed.black('错误'), chalk.red(msg));
+        print(chalk.bgRed.black('错误'), printLog(msg,'red'));
     },
     success: (msg) => {
-        print(chalk.bgGreen.black('成功'), chalk.green(msg));
+        print(chalk.bgGreen.black('成功'), printLog(msg,'green'));
     }
 };
+function printLog(msg, color) {
+    if ( typeof msg === 'object') {
+        return msg;
+    } else {
+        return chalk[color](msg);
+    }
+   
+}
 module.exports = log;
